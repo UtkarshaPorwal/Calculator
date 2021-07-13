74820203d4773ccf352a992cb7289e7fb28f65ac
UtkarshaPorwal <86833971+UtkarshaPorwal@users.noreply.github.com>
2021-07-13 09:46:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+    document.getElementById("result").value+=val
+            }
+            function solve()
+         {
+             let x = document.getElementById("result").value
+             let y = eval(x)
+             document.getElementById("result").value = y
+         }
+         function clr()
+         {
+             document.getElementById("result").value = ""
+         }
